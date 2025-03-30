610a7b0551398d495393a9e22762902630a6629a
Anurag Kumar Pathak <143236701+Anurag-pat@users.noreply.github.com>
2025-03-30 23:40:14 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,60 +1,22 @@
-import nltk
-nltk.download('punkt')
-nltk.download('punkt_tab')
-nltk.download('stopwords')
-
 import streamlit as st
-import pickle 
-import string
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
+import pickle
 
-ps = PorterStemmer()
+model = pickle.load(open('spam.pkl','rb'))
+cv = pickle.load(open('vectorizer.pkl','rb'))
 
 
-def transform_text(text):
-    text = text.lower()
-    text = nltk.word_tokenize(text)
+st.title("Email Spam Classification application")
+st.write("This is a Machine Learning application to classify emails as spam or ham.")
+user_input= st.text_area("Enter an email to classify",height=150)
 
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
-
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        y.append(ps.stem(i))
-
-    return " ".join(y)
-
-
-tk = pickle.load(open("vectorizer.pkl", 'rb'))
-model = pickle.load(open("model.pkl", 'rb'))
-
-st.title("SMS Spam Detection Model")
-st.write("*Made by Anurag Kumar Pathak*")    
-
-input_sms = st.text_input("Enter the SMS")
-
-if st.button('Predict'):
-
-    # 1. preprocess
-    transformed_sms = transform_text(input_sms)
-    # 2. vectorize
-    vector_input = tk.transform([transformed_sms])
-    # 3. predict
-    result = model.predict(vector_input)[0]
-    # 4. Display
-    if result == 1:
-        st.header("Spam")
+if st.button("Classify") :
+    if user_input:
+        data = [user_input]
+        vectorized_data = cv.transform(data).toarray()
+        result = model.predict(vectorized_data)
+        if result[0]==0:
+            st.write("The email is not spam")
+        else:
+            st.write("The email is spam")
     else:
-        st.header("Not Spam")
+        st.write("Please type Email to classify")
